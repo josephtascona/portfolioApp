e33cc267864477aa12964b412a548615f6aa4904
Joseph Tascona <joseph.tascona@gmail.com>
2022-10-06 16:05:37 -0400

    Added nodemailer and dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +8,28 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+function main(from, subject, text) {
+  // nodemailer transporter
+  var transporter = nodemailer.createTransport({
+    port: 465,               // true for 465, false for other ports
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+    secure: true,
+  });
+
+  transporter.sendMail({
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: subject,
+    text: "From: " + from + "\nMessage: \n" + text,
+  });
+}
+
+router.post("/", function (req, res, next) {
+  main(req.body.from, req.body.subject, req.body.text);
+  res.render('index', { title: 'Portfolio' });
+})
